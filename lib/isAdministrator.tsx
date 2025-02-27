cc9137ab4e6d7328ae7d003f3158abebb0eefc61
Heri Roque <heriroque.22@gmail.com>
2025-02-27 11:48:27 -0500

    feat: update isAdministrator function to support multiple admin users

--- lib/isAdministrator.tsx
+++ lib/isAdministrator.tsx
@@ -1,3 +1,5 @@
 export const isAdministrator = (userId: string | null | undefined) => {
-  return userId === process.env.NEXT_PUBLIC_ADMINISTRATOR;
+  const administrators =
+    process.env.NEXT_PUBLIC_ADMINISTRATOR?.split(",") || [];
+  return administrators.includes(userId ?? "");
 };
